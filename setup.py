a6ef0457754eb92b08a15befc55c3edc1f868bc4
Willem Spruijt <willem.spruijt@gmail.com>
2013-08-12 15:49:55 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='Fosbury',
-      version='0.8.3',
+      version='0.9',
       author='Willem Spruijt',
       author_email='willem@fosbury.co',
       description='Python client for Fosbury (http://fosbury.co)',
-      packages=find_packages())
+      packages=find_packages(),
+      license='Apache License 2.0')
